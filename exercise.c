458ecb9b46f7237fe445a71d7d48f9106acacdc7
saladinhodzic <hsaladin06@gmail.com>
2024-11-04 20:26:58 +0100

    C finally adjusted, exercise done with finding o and p of rectangle

--- exercise.c
+++ exercise.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+
+// izracunaj obim i povrsinu pravugaunika na osnovu dva parametra
 int main(){
-    printf("Hello world!");
+    float a,b,o,p;
+    printf("Stranica a ");
+    scanf("%f",&a);
+    printf("Unesi stranicu b ");
+    scanf("%f",&b);
+    o = 2*a + 2*b;
+    printf("Obim je %.2f",o);
+    p = a*b;
+    printf("Povrsina je %.2f",p);
+    // getche();
+
+
     return 0;
 }
