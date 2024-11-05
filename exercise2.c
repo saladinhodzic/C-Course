0e7d31bdca400a0e379c27203d5362993c810e6d
saladinhodzic <hsaladin06@gmail.com>
2024-11-05 18:57:24 +0100

    Tried to make an array but it doesnt work

--- exercise2.c
+++ exercise2.c
@@ -24,10 +24,20 @@ int main (){
     // printf("Povrsina valjka je %.2f i zapremina je %.2f",p,v);
     // 2.20. Саставити програм којим се вредност темепературе унете у Фаренхајтима приказује у
 // Целзијусима
-    float F,C;
-    printf("Unesi stepene u Farehajtovima\n");
-    scanf("%f",&F);
-    C=(F-32)*5/9;
-    printf("Stepeni u celzijusu su %.2f",C);
+    // float F,C;
+    // printf("Unesi stepene u Farehajtovima\n");
+    // scanf("%f",&F);
+    // C=(F-32)*5/9;
+    // printf("Stepeni u celzijusu su %.2f C",C);
+    // 1.	Sa tastature se učitava prirodan broj n i zatim n prirodnih brojeva među kojima su bar dva različita. Napisati program koji jednim prolaskom kroz niz određuje drugu po veličini vrednost. Na primer za pet brojeva 6, 6, 7, 6, 2 ispisuje:
+
+    int n,br,i;
+    printf("Unesi n prirodnih brojeva medju kojima su bar dva razlicita");
+    scanf("%d",&n);
+    int list[5];
+    for(i=0;i<list[n];i++){
+        scanf("%d",&list[i]);
+    }
+    printf("%d",list[5]);
     return 0;
 }
