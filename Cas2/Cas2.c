937745b20ca5442f9ec3b7ff0a4efc2ed385d144
saladinhodzic <hsaladin06@gmail.com>
2024-11-15 17:12:04 +0100

    Rekurzivna funkcija za izracunavanje stepena broja

--- Cas2/Cas2.c
+++ Cas2/Cas2.c
@@ -2,20 +2,40 @@
 // 9.29. Саставити рекурзивну функцију за одређивање факторијела целог броја, а затим је тестирати
 // у главном програму за унети цео број n и исписати добијени резултат. 
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int faktorijel(int n){
-    if (n==1){
+// int faktorijel(int n){
+//     if (n==1){
+//         return n;
+//     }
+//     return n*faktorijel(n-1);
+// }
+
+// int main(){
+//     int n;
+//     printf("Uneti ceo broj n i onda cemo naci njegov faktorijel ");
+//     scanf("%d",&n);
+    
+//     printf("Za uneti broj faktorijel je %d",faktorijel(n));
+//     return 0;
+// }
+
+// 9.30. Саставити рекурзивну функцију која степеновање целог броја на целобројни изложилац, а
+// затим тестирати функцију за дати природни број и изложилац и исписати добијени резултат. 
+
+
+#include <stdio.h>
+int stepen(int n,int s){
+    if (s==1){
         return n;
     }
-    return n*faktorijel(n-1);
+    return n * stepen(n,s-1);
 }
 
 int main(){
-    int n;
-    printf("Uneti ceo broj n i onda cemo naci njegov faktorijel ");
-    scanf("%d",&n);
-    
-    printf("Za uneti broj faktorijel je %d",faktorijel(n));
+    int n,s;
+    printf("Uneti prirodan broj n i stepen s ");
+    scanf("%d %d",&n,&s);
+    printf("Stepen datog broja je %d",stepen(n,s));
     return 0;
 }
