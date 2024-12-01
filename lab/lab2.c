badd3eff33a55d8d618172271cbdb28fe9222814
saladinhodzic <hsaladin06@gmail.com>
2024-12-01 16:28:20 +0100

    Pronalazenje aritmeticke sredine u datom nizu

--- lab/lab2.c
+++ lab/lab2.c
@@ -8,43 +8,60 @@
 
 // NACI ZA UNETI BROJ K ODGOVARAJACU CIFRU U NIZU
 
-#include <stdio.h>
-#include <math.h>
+// #include <stdio.h>
+// #include <math.h>
 
-int generate(int niz[],int length,int noviNiz[]){
-    int i,acc,j,m=1,k;
-    noviNiz[100];
-    for(i=0;i<length;i++){
-        acc=round(pow(10,i));
-        niz[i]=acc;
-    }
-    for (j=0;j<length;j++){
-        while(niz[j]>0){
-            int remainder=niz[j]%10;
-            niz[j]/=10;
-            noviNiz[m]=remainder;
-            m++;
-        }
-    }
-    return noviNiz;
-}
+// int generate(int niz[],int length,int noviNiz[]){
+//     int i,acc,j,m=1,k;
+//     noviNiz[100];
+//     for(i=0;i<length;i++){
+//         acc=round(pow(10,i));
+//         niz[i]=acc;
+//     }
+//     for (j=0;j<length;j++){
+//         while(niz[j]>0){
+//             int remainder=niz[j]%10;
+//             niz[j]/=10;
+//             noviNiz[m]=remainder;
+//             m++;
+//         }
+//     }
+//     return noviNiz;
+// }
 
-int main(){
-    int niz[10],i,noviNiz[100]={1};
-    int length=sizeof(niz)/sizeof(niz[0]);
-    generate(niz,length,noviNiz);
+// int main(){
+//     int niz[10],i,noviNiz[100]={1};
+//     int length=sizeof(niz)/sizeof(niz[0]);
+//     generate(niz,length,noviNiz);
 
 
-    int pozicija;
-    printf("Unesite neki indeks i mi cemo ti vratiti cifru sa tog indeksa\n");
-    scanf("%d",&pozicija);
+//     int pozicija;
+//     printf("Unesite neki indeks i mi cemo ti vratiti cifru sa tog indeksa\n");
+//     scanf("%d",&pozicija);
 
-    for(i=0;i<length;i++){
-        if (i==pozicija){
-            printf("Na %d. poziciji se nalazi cifra %d.",pozicija,noviNiz[i]);
-        }
-    }
+//     for(i=0;i<length;i++){
+//         if (i==pozicija){
+//             printf("Na %d. poziciji se nalazi cifra %d.",pozicija,noviNiz[i]);
+//         }
+//     }
 
     
+//     return 0;
+// }
+
+
+// Саставити програм за израчунавање и исписивање аритметичке средине задатог низа (дужине
+// n) целих бројева. 
+
+#include <stdio.h>
+
+int main(){
+    int niz[5]={1,2,3,4,5},i,suma=0;
+    int length=sizeof(niz)/4;
+    for (i=0;i<length;i++){
+        suma+=niz[i];
+    }
+    float result=suma/length;
+    printf("Aritmeticka sredina brojeva niza je %.2f",result);
     return 0;
 }
