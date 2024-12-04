9ae64b7e094e9e0f3be31a2fcd72e715bec26816
saladinhodzic <hsaladin06@gmail.com>
2024-12-04 08:44:38 +0100

    Fibonacci sequence

--- lab/lab2.c
+++ lab/lab2.c
@@ -462,38 +462,75 @@
 // = ∑ , а затим саставити главни програм који ће учитати два низа једнаких дужина n и
 // применом формирани функције исписати скаларни производ два низа. 
 
+// #include <stdio.h>
+
+// int proizvod(int A[],int B[],int duzina);
+
+// int main(void)
+// {
+//     int A[100],B[100],n,i;
+
+//     printf("Uneti duzinu nizeva: ");
+//     scanf("%d",&n);
+
+//     for(i=0;i<n;i++)
+//     {
+//         scanf("%d",&A[i]);
+//     }
+//     for(i=0;i<n;i++)
+//     {
+//         scanf("%d",&B[i]);
+//     }
+
+//     printf("%d",proizvod(A,B,n));
+
+    
+//     return 0;
+// }
+
+// int proizvod(int A[],int B[],int duzina)
+// {
+//     int i,suma=0;
+//     for(i=0;i<duzina;i++)
+//     {
+//         suma+=A[i]*B[i];
+//     }
+//     return suma;
+// }
+
+// Саставити функцију која генерише првих n чланова Фибоначијевог низа и функцију која
+// исписује чланове низа, а затим те функције тестирати у главном програму. 
+
 #include <stdio.h>
 
-int proizvod(int A[],int B[],int duzina);
+void fibonacijevNiz(int n);
 
 int main(void)
 {
-    int A[100],B[100],n,i;
-
-    printf("Uneti duzinu nizeva: ");
+    int n;
+    printf("Unesite duzinu fibonacijevog niza: ");
     scanf("%d",&n);
-
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&A[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        scanf("%d",&B[i]);
-    }
-
-    printf("%d",proizvod(A,B,n));
-
-    
+    fibonacijevNiz(n);
     return 0;
 }
 
-int proizvod(int A[],int B[],int duzina)
+void fibonacijevNiz(int n)
 {
-    int i,suma=0;
-    for(i=0;i<duzina;i++)
+    int i,a=0,b=1,c;
+    for(i=0;i<n;i++)
     {
-        suma+=A[i]*B[i];
+        if(i==0)
+        {
+            printf("%d, ",i);
+        }else if(i==1)
+        {
+            printf("%d, ",i);
+        }else
+        {
+            c=a+b;
+            printf("%d, ",c);
+            a=b;
+            b=c;
+        }
     }
-    return suma;
 }
