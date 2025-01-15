1f4e7218f858f45fd876ca0dfc8786171a08004f
saladinhodzic <hsaladin06@gmail.com>
2025-01-15 20:23:02 +0100

    Implement program to find the largest number in an array of user-defined length

--- zbirka1/zbirka1.c
+++ zbirka1/zbirka1.c
@@ -14,17 +14,53 @@
 // char f;
 // написати функцију која учитава вредности са тастатуре за следеће променљиве:
 
-#include <stdio.h>
+// #include <stdio.h>
 
+// int main(void)
+// {
+//     int a, b;
+//     long int i, j;
+//     double x, y;
+//     char f;
+
+//     scanf("%d %d",&a,&b);
+//     scanf("%ld %ld",&i,&j);
+//     scanf("%lf %lf",&x,&y);
+//     scanf("%c",&f);
+// }
+
+// Napisati program koji nalazi najveci broj u nizu od 10 brojeva
+
+#include <stdio.h>
+void formiraj(int niz[],int n);
+void najveci(int niz[],int n);
 int main(void)
 {
-    int a, b;
-    long int i, j;
-    double x, y;
-    char f;
+    int niz[100],n;
 
-    scanf("%d %d",&a,&b);
-    scanf("%ld %ld",&i,&j);
-    scanf("%lf %lf",&x,&y);
-    scanf("%c",&f);
+    printf("Unesite duzinu n: ");
+    scanf("%d",&n);
+    formiraj(niz,n);
+    najveci(niz,n);
+}
+
+void formiraj(int niz[],int n)
+{
+    int i;
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&niz[i]);
+    }
+}
+void najveci(int niz[],int n)
+{
+    int najveci = niz[0];
+    for (int i=0;i<n;i++)
+    {
+        if (niz[i]>najveci)
+        {
+            najveci = niz[i];
+        }
+    }
+    printf("Najveci broj u nizu je %d.",najveci);
 }
