44195744043f17f8a4a9b2a8d009d33ffc41f618
saladinhodzic <hsaladin06@gmail.com>
2025-01-22 14:10:26 +0100

    Refactor string length determination program; implement comparison of two strings' lengths and remove unused code

---
+++ zbirka2/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+int main(void)
+// {
+//     char str[]="Cao";
+//     int length = sizeof(str) - 1;
+//     printf("%d",length);
+// }
--- zbirka2/zbirka2.c
+++ zbirka2/zbirka2.c
@@ -3,13 +3,57 @@
 Matrice, Stringovi, Pokazivaci, Strukture, Datoteke. 
 */
 
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char knjiga[100][100] = {"Operatori","Ciklusi","Skokovi","Karakteri","Nizovi","Matrice","Stringovi","Pokazivaci","Strukture","Datoteke"};
+//     int n;
+//     printf("Unesite poglavlje n (1-10): ");
+//     scanf("%d",&n);
+//     printf("%s",knjiga[n-1]);
+// }
+
+/*Саставити програм за одређивање дужине унетог стринга (без null карактера):
+a) без употребе уграђене функције strlen();
+б) употребом уграђене функције strlen();
+в) саставити програм за одређивање који је дужи од два задата стринга (без null карактера)
+употребом уграђене функције strlen(); 
+*/
+
 #include <stdio.h>
+
+// a)
+
+// int main(void)
+// {
+//     char str[]="Cao";
+//     int length = sizeof(str) - 1;
+//     printf("%d",length);
+// }
+
+// b)
+// #include <string.h>
+// int main(void)
+// {
+//     char str[] = "cao";
+//     int length = strlen(str);
+//     printf("%d",length);
+// }
+
+// v)
+
+#include <string.h>
 
 int main(void)
 {
-    char knjiga[100][100] = {"Operatori","Ciklusi","Skokovi","Karakteri","Nizovi","Matrice","Stringovi","Pokazivaci","Strukture","Datoteke"};
-    int n;
-    printf("Unesite poglavlje n (1-10): ");
-    scanf("%d",&n);
-    printf("%s",knjiga[n-1]);
+    char str1[] = "cao cao";
+    char str2[] = "cao";
+    if(strlen(str1)>strlen(str2))
+    {
+        printf("Prvi string je duzi.");
+    }else
+    {
+        printf("Drugi string je duzi.");
+    }
 }
